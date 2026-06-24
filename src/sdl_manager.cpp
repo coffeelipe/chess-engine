f1c362500cba950e361be8c1ff85a46425c9c0a1
Felipe Pontes <fpontes321@gmail.com>
2026-06-24 16:12:30 -0300

    Implement method for drawing a chess board on screen

--- src/sdl_manager.cpp
+++ src/sdl_manager.cpp
@@ -23,3 +23,65 @@ bool SDLManager::init()
 
     return true;
 }
+
+void SDLManager::drawBoard()
+{
+    float rowSize = 600;
+    float squareSize = rowSize / 8;
+    float squareCoordinateX = 0;
+    float rowCoordinateY = 0;
+
+    SDL_Texture *boardTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, rowSize, squareSize);
+    SDL_SetRenderTarget(renderer, boardTexture);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+
+    for (int i = 0; i <= 7; i++)
+    {
+        if (i % 2 == 0)
+        {
+            SDL_SetRenderDrawColor(renderer, 33, 65, 50, 255);
+        }
+        else
+        {
+
+            SDL_SetRenderDrawColor(renderer, 223, 220, 221, 255);
+        }
+
+        SDL_FRect currentSquare = {squareCoordinateX, 0, squareSize, squareSize};
+        SDL_RenderFillRect(renderer, &currentSquare);
+        squareCoordinateX += squareSize;
+    }
+
+    SDL_SetRenderTarget(renderer, nullptr);
+    SDL_SetTextureBlendMode(boardTexture, SDL_BLENDMODE_BLEND);
+
+    SDL_FRect *src = nullptr;
+
+    SDL_FRect dest;
+    dest.x = 100;
+    dest.y = 0;
+    dest.w = rowSize;
+    dest.h = squareSize;
+
+    for (int i = 0; i <= 7; i++)
+    {
+        if (i % 2 == 0)
+        {
+            SDL_RenderTextureRotated(
+                renderer,
+                boardTexture,
+                src,
+                &dest,
+                0.0,
+                nullptr,
+                SDL_FLIP_HORIZONTAL);
+        }
+        else
+        {
+            SDL_RenderTexture(renderer, boardTexture, nullptr, &dest);
+        }
+
+        dest.y += squareSize;
+    }
+}
