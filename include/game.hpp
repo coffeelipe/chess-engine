580c855f85b3ad755ff9b6774870690430d2da0f
Felipe Pontes <fpontes321@gmail.com>
2026-06-24 15:53:59 -0300

    Implement the Game class header

---
+++ include/game.hpp
@@ -0,0 +1,19 @@
+#include "sdl_manager.hpp"
+#include "sdl_fwd.hpp"
+#include "board.hpp"
+
+class Game
+{
+public:
+    void run();
+
+private:
+    bool init();
+    void handleEvent(const SDL_Event &event);
+    void onQuit();
+    void render(SDL_Window *window, SDL_Renderer *renderer);
+
+    bool isRunning = true;
+    SDLManager sdl;
+    Board board;
+};
