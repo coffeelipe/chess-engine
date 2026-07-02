c46688e02496a7d8813e63cf3cbbe1657b671a68
Felipe Pontes <fpontes321@gmail.com>
2026-07-01 23:36:54 -0300

    Add methods for loading and cleaning textures from memory

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "common.hpp"
 #include "sdl_fwd.hpp"
 
 class Renderer
@@ -8,12 +8,16 @@ public:
     explicit Renderer(SDL_Renderer *renderer = nullptr);
 
     void setRenderer(SDL_Renderer *renderer);
-    void drawBoard();
-    void renderPresent();
 
-    private:
+    void drawBoard();
+    bool loadPieceTextures();
+    void renderPresent();
+    void destroyTextures();
+
+private:
     SDL_Texture *createRowTexture();
     void renderRowTexture(SDL_Texture *rowTexture, int rowIndex);
+    std::map<std::string, SDL_Texture *> pieces{};
 
     SDL_Renderer *renderer = nullptr;
 };
